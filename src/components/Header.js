349db6d87bfb3dca720c66196c02404f04106e07
Huyenpta <huyenpta1990@gmail.com>
2025-09-18 18:13:55 +0700

    BT

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import { Navbar, Nav, Button, Container } from 'react-bootstrap';
+import '../App.css';
 
 const Header = () => {
   return (
@@ -6,21 +7,22 @@ const Header = () => {
       <Container>
         <Navbar.Brand href="#home">
           <img
-            src="images/logo.jpg"  
+            src="images/logo.jpg"
             width="30"
             height="30"
             className="d-inline-block align-top"
             alt="FPT Aptech logo"
-          />{' '} 
+          />{' '}
           FPT Aptech
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#about">About Us</Nav.Link>
-            <Nav.Link href="#products">Products</Nav.Link>
-            <Nav.Link href="#booking">Booking</Nav.Link>
-            <Nav.Link href="#locations">Locations</Nav.Link>
+            {/* Thêm class nav-link-hover vào đây */}
+            <Nav.Link href="#about" className="nav-link-hover">About Us</Nav.Link>
+            <Nav.Link href="#products" className="nav-link-hover">Products</Nav.Link>
+            <Nav.Link href="#booking" className="nav-link-hover">Booking</Nav.Link>
+            <Nav.Link href="#locations" className="nav-link-hover">Locations</Nav.Link>
           </Nav>
           <Button variant="warning" className="me-2">Sign In</Button>
           <Button variant="outline-primary">Sign Up</Button>
