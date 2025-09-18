a9573ce7ed6f5d7831acb8a6e2d2b0d22c03f347
Huyenpta <huyenpta1990@gmail.com>
2025-09-18 17:38:42 +0700

    BT

--- src/context/context.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const GlobalContext = React.createContext();//tạo global State nhà kho
-export const GlobalProvider = GlobalContext.Provider;
-export default GlobalContext;
--- src/context/initState.js
+++
@@ -1,7 +0,0 @@
-const savedCart = localStorage.getItem("cart");
-
-const STATE = {
-  cart: savedCart ? JSON.parse(savedCart) : []  // lấy lại giỏ hàng cũ
-};
-
-export default STATE;
